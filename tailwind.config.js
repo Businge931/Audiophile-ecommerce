b12fdabdcf9f3a6b986ab165af856eb0265cac3b
Businge931 <jimmybusinge9@gmail.com>
2023-05-22 00:43:24 +0300

    set up project structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx}"],
+  mode: "jit",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#D87D4A",
+        secondary: "#fbaf85",
+        tertiary: "#000000",
+        white: "#FFFFFF",
+        "white-100": "#FAFAFA",
+        "white-200": "#F1F1F1",
+        black: "#000000",
+        "black-100": "#101010",
+      },
+    },
   },
   plugins: [],
-}
-
+};
