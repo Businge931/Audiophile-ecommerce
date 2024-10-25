fb61e7f55556cc35aee57a0e3c0efeef795ddba3
Businge931 <jimmybusinge9@gmail.com>
2024-10-25 23:20:47 +0300

    add background imagess

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,20 @@ module.exports = {
         white: "#FFFFFF",
         "white-100": "#FAFAFA",
         "white-200": "#F1F1F1",
+        "white-300": "#F2F2F2",
         black: "#000000",
         "black-100": "#101010",
       },
+      backgroundImage: {
+        "home-bg": "url('./assets/home/desktop/image-hero.jpg')",
+        "home-bg-tablet": "url('./assets/home/tablet/image-header.jpg')",
+        "zx7-speaker": "url('./assets/home/desktop/image-speaker-zx7.jpg')",
+        "zx7-speaker-tablet":
+          "url('./assets/home/tablet/image-speaker-zx7.jpg')",
+      },
+    },
+    letterSpacing: {
+      1: "10px",
     },
   },
   plugins: [],
