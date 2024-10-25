c634883de7ba8c242cb3e1f4ca84d428ec694455
Businge931 <jimmybusinge9@gmail.com>
2024-10-25 23:30:54 +0300

    esport product details page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,4 @@ export { default as Home } from "./Home";
 export { default as Headphones } from "./Headphones";
 export { default as Earphones } from "./Earphones";
 export { default as Speakers } from "./Speakers";
+export { default as ProductDetails } from "./ProductDetails";
