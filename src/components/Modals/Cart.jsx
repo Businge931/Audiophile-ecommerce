f750b8101743e9c1ee3187b0dffba7e92c44817d
Businge931 <jimmybusinge9@gmail.com>
2024-10-25 23:26:38 +0300

    add cart

---
+++ src/components/Modals/Cart.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cart = () => {
+  return (
+    <div className='w-screen h-screen bg-black opacity-60'>
+        <div></div>
+    </div>
+  )
+}
+
+export default Cart
