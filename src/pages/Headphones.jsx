2a392bb56c1732e70b184e471a451ad546c3e388
Businge931 <jimmybusinge9@gmail.com>
2024-10-25 23:29:45 +0300

    display headphones pages

--- src/pages/Headphones.jsx
+++ src/pages/Headphones.jsx
@@ -1,7 +1,56 @@
 import React from "react";
+import Navbar from "../components/Navbar";
+import AboutProduct from "../components/AboutProduct";
+
+import headphon1 from "../assets/product-xx99-mark-two-headphones/desktop/image-product.jpg";
+import headphon2 from "../assets/product-xx99-mark-one-headphones/desktop/image-product.jpg";
+import headphon3 from "../assets/product-xx59-headphones/desktop/image-product.jpg";
+
+import BestAudioGear from "../components/BestAudioGear";
+import Footer from "../components/Footer";
+import ShopProductsContainer from "../components/ShopProductsContainer";
 
 const Headphones = () => {
-  return <div>Headphones</div>;
+  return (
+    <>
+      <header className="bg-black relative h-60">
+        <Navbar />
+        <h1 className="text-white uppercase text-center py-8 text-[40px] font-bold">
+          Headphones
+        </h1>
+      </header>
+      <section className="my-8">
+        <AboutProduct
+          subheading="New product"
+          heading="XX99 Mark II Headphones"
+          content="The new XX99 Mark II headphones is the pinnacle of pristine 
+              audio. It redefines your premium headphone experience by reproducing 
+             the balanced depth and precision of studio-quality sound."
+          headphoneImage={headphon1}
+        />
+
+        <AboutProduct
+          reverse
+          heading="XX99 Mark I Headphones"
+          content="As the gold standard for headphones, the classic XX99 
+          Mark I offers detailed and accurate audio reproduction for audiophiles, 
+          mixing engineers, and music aficionados alike in studios and on the go."
+          headphoneImage={headphon2}
+        />
+        <AboutProduct
+          heading="XX59 Headphones"
+          content="Enjoy your audio almost anywhere and customize it 
+          to your specific tastes with the XX59 headphones. The stylish yet durable 
+          versatile wireless headset is a brilliant companion at home or on the move."
+          headphoneImage={headphon3}
+        />
+      </section>
+
+      <ShopProductsContainer />
+      <BestAudioGear />
+      <Footer />
+    </>
+  );
 };
 
 export default Headphones;
